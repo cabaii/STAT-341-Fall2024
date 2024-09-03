9322b47a64a8b79d0e025b8e4b903414445432da
cabaii <136500761+cabaii@users.noreply.github.com>
2024-09-03 16:23:27 -0400

    Add files via upload

---
+++ Assignment 3 stat 341.R
@@ -0,0 +1,16 @@
+Author: Abai; Date: Aug 30, 2024; Purpose: Calculate the correlation coefficient test
+
+
+sample(1000)
+
+variablel <- runif(1000)
+
+
+sd(variablel)
+[1] 0.2972549
+
+mean(variablel)
+[1] 0.492791
+
+median(variablel)
+[1] 0.4824829
