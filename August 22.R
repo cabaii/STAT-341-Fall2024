f12013c85f0e539a34c11a2f565742a5fb2346d0
cabaii <136500761+cabaii@users.noreply.github.com>
2024-09-19 15:58:28 -0400

    Add files via upload

---
+++ August 22.R
@@ -0,0 +1,24 @@
+#Author: Abai ikwechegh; August 22 2024; Purpose: Take the measure of desciptive stats
+
+# name() = function
+# run code in console, if quit, anything ran is deleted
+
+#need data / input for a function
+
+X<- c(1,2,3,4,5)
+
+# we can append x and add 6 by writing  : X<-6
+
+Y <-100
+
+#How to calculate standard deviation in R
+sd(X)
+
+#How to calculate mean in R
+mean(X)
+
+#How to calculate median in R
+Median(X)
+
+#Mode is not present in R language
+
